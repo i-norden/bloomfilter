2895c58d1c115973b0a35444f2edee828f3ab0bf
i-norden <iansnorden@gmail.com>
2024-01-26 16:27:59 -0500

    secure hasher test

--- hasher_test.go
+++ hasher_test.go
@@ -9,7 +9,9 @@ import (
 func TestHasher(t *testing.T) {
 	for _, hash := range defaultHashers {
 		array1 := []byte{1, 2, 3}
-		if !reflect.DeepEqual(hash(array1), hash(array1)) {
+		_, h1 := hash(array1)
+		_, h2 := hash(array1)
+		if !reflect.DeepEqual(h1, h2) {
 			t.Error("undeterministic")
 		}
 	}
@@ -29,7 +31,9 @@ func BenchmarkHasher(b *testing.B) {
 func TestDefaultHashFactory(t *testing.T) {
 	for _, hash := range DefaultHashFactory(23) {
 		array1 := []byte{1, 2, 3}
-		if !reflect.DeepEqual(hash(array1), hash(array1)) {
+		_, h1 := hash(array1)
+		_, h2 := hash(array1)
+		if !reflect.DeepEqual(h1, h2) {
 			t.Error("undeterministic")
 		}
 	}
@@ -38,7 +42,20 @@ func TestDefaultHashFactory(t *testing.T) {
 func TestOptimalHashFactory(t *testing.T) {
 	for _, hash := range OptimalHashFactory(23) {
 		array1 := []byte{1, 2, 3}
-		if !reflect.DeepEqual(hash(array1), hash(array1)) {
+		_, h1 := hash(array1)
+		_, h2 := hash(array1)
+		if !reflect.DeepEqual(h1, h2) {
+			t.Error("undeterministic")
+		}
+	}
+}
+
+func TestSecureHashFactory(t *testing.T) {
+	for _, hash := range SecureHashFactory(23) {
+		array1 := []byte{1, 2, 3}
+		_, h1 := hash(array1)
+		_, h2 := hash(array1)
+		if !reflect.DeepEqual(h1, h2) {
 			t.Error("undeterministic")
 		}
 	}
@@ -46,6 +63,17 @@ func TestOptimalHashFactory(t *testing.T) {
 
 func BenchmarkOptimalHashFactory(b *testing.B) {
 	for k, hash := range OptimalHashFactory(23) {
+		b.Run(fmt.Sprintf("hasher %d", k), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				array1 := []byte{1, 2, 3}
+				hash(array1)
+			}
+		})
+	}
+}
+
+func BenchmarkSecureHashFactory(b *testing.B) {
+	for k, hash := range SecureHashFactory(23) {
 		b.Run(fmt.Sprintf("hasher %d", k), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				array1 := []byte{1, 2, 3}
